d8f185aa17c8013c4fb47b5a9aff3aad89de35e1
ziejaCode <ziejacode@gmail.com>
2020-12-24 10:53:48 +0000

    uodate test_employee

--- unittesting/test_emoloyee.py
+++ unittesting/test_emoloyee.py
@@ -4,6 +4,9 @@ from employee import Employee
 
 
 class TestEmployee(unittest.TestCase):
+    
+
+
 
     @classmethod
     def setUpClass(cls):
@@ -12,7 +15,7 @@ class TestEmployee(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         print('teardownClass')
-
+    
     def setUp(self):
         print('setUp')
         self.emp_1 = Employee('Corey', 'Schafer', 50000)
@@ -20,7 +23,7 @@ class TestEmployee(unittest.TestCase):
 
     def tearDown(self):
         print('tearDown\n')
-
+    
     def test_email(self):
         print('test_email')
         self.assertEqual(self.emp_1.email, 'Corey.Schafer@email.com')
@@ -48,7 +51,7 @@ class TestEmployee(unittest.TestCase):
         self.emp_1.apply_raise()
         self.emp_2.apply_raise()
 
-        self.assertEqual(self.emp_1.pay, 52500)
+        self.assertEqual(self.emp_1.pay, 52500) 
         self.assertEqual(self.emp_2.pay, 63000)
 
     def test_monthly_schedule(self):
@@ -66,6 +69,28 @@ class TestEmployee(unittest.TestCase):
             mocked_get.assert_called_with('http://company.com/Smith/June')
             self.assertEqual(schedule, 'Bad Response!')
 
+
+
+# test employee
+'''
+import employee
+
+print(' I am for testing')
+    def test_monthly_schedule(self):
+        with patch('employee.requests.get') as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = 'Success'
+
+            schedule = self.emp_1.monthly_schedule('May')
+            mocked_get.assert_called_with('http://company.com/Schafer/May')
+            self.assertEqual(schedule, 'Success')
+
+            mocked_get.return_value.ok = False
+
+            schedule = self.emp_2.monthly_schedule('June')
+            mocked_get.assert_called_with('http://company.com/Smith/June')
+            self.assertEqual(schedule, 'Bad Response!')
+'''
 
 if __name__ == '__main__':
     unittest.main()
