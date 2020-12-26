e53193223ae15f6fbb5c479816db0f16b3db0fa8
ziejaCode <ziejaCode@gmail.com>
2020-12-26 10:05:54 +0000

    update 2

--- unittesting/test_emoloyee.py
+++ unittesting/test_emoloyee.py
@@ -71,26 +71,7 @@ class TestEmployee(unittest.TestCase):
 
 
 
-# test employee
-'''
-import employee
 
-print(' I am for testing')
-    def test_monthly_schedule(self):
-        with patch('employee.requests.get') as mocked_get:
-            mocked_get.return_value.ok = True
-            mocked_get.return_value.text = 'Success'
-
-            schedule = self.emp_1.monthly_schedule('May')
-            mocked_get.assert_called_with('http://company.com/Schafer/May')
-            self.assertEqual(schedule, 'Success')
-
-            mocked_get.return_value.ok = False
-
-            schedule = self.emp_2.monthly_schedule('June')
-            mocked_get.assert_called_with('http://company.com/Smith/June')
-            self.assertEqual(schedule, 'Bad Response!')
-'''
 
 if __name__ == '__main__':
     unittest.main()
